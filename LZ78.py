d30eaa72f6371ca5792bc6b0a80489619b241bbc
Igor Rahzel <igorrahzel@gmail.com>
2023-05-07 08:11:26 -0300

    lendo arquivo

--- LZ78.py
+++ LZ78.py
@@ -1,7 +1,7 @@
 from TrieTree import Node
 from math import log2,ceil
 
-def lz78_compression(string,firstLook = False,bitsSize = None):
+def lz78_compression(text,firstLook = False,bitsSize = None):
 
     with open("myfile.txt", "w") as f:
         f.write('')
@@ -17,35 +17,36 @@ def lz78_compression(string,firstLook = False,bitsSize = None):
     counter = 0 
     num_nodes = 0
 
-    n = len(string)
-    i = 0
-
-    while i < n:
-        counter = 0
-        aux = root.insert(string[i],value = num_nodes + 1, firstLook = firstLook,bitSize = bitsSize)
-        #character wasn't a child of the root node
-        if aux == None:
-            num_nodes += 1
-            i+=1
-        #character was a child of the root node
-        else:
-            while aux != None:
-                if i < len(string) -1 :
-                    new_letter = string[i+1] 
+    with open(text,'r') as file:
+        for line in file:
+            n = len(line)
+            i = 0
+            while i < n:
+                counter = 0
+                aux = root.insert(line[i],value = num_nodes + 1, firstLook = firstLook,bitSize = bitsSize)
+                #character wasn't a child of the root node
+                if aux == None:
+                    num_nodes += 1
+                    i+=1
+                #character was a child of the root node
                 else:
-                    new_letter = '\0'
-                #current charater and next
-                if counter == 0:
-                    word = aux + new_letter
-                #multiple characters
-                else:
-                    word += new_letter
-                aux = root.insert(word,value = num_nodes + 1,firstLook = firstLook, bitSize = bitsSize)
-                counter += 1
-                i+=1
-            i += 1
-            num_nodes += 1
-    
+                    while aux != None:
+                        if i < len(line) -1 :
+                            new_letter = line[i+1] 
+                        else:
+                            new_letter = '\0'
+                        #current charater and next
+                        if counter == 0:
+                            word = aux + new_letter
+                        #multiple characters
+                        else:
+                            word += new_letter
+                        aux = root.insert(word,value = num_nodes + 1,firstLook = firstLook, bitSize = bitsSize)
+                        counter += 1
+                        i+=1
+                    i += 1
+                    num_nodes += 1
+            
     if firstLook == False:
         return num_nodes
 
@@ -109,11 +110,12 @@ def lz78_decompression(file):
 
 #PARTE DA COMPRESSÃO
 string = 'sir sid eastman easily teases sea sick seals'
-num_nodes = lz78_compression(string)
+text = 'example.txt'
+num_nodes = lz78_compression(text)
 num_nodes_log_2 = log2(num_nodes)
 num_nodes_log_2 = ceil(num_nodes_log_2)
 print(num_nodes_log_2)
-lz78_compression(string,firstLook=True,bitsSize = num_nodes_log_2)
+lz78_compression(text,firstLook=True,bitsSize = num_nodes_log_2)
 
 #PARTE DA DESCOMPRESSÃO
 lz78_decompression('myfile.txt')
